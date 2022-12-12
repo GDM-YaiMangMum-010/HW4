1dcb63cc715a489f8ff2451dbde1776b720b4ddd
Jirayu_SBTR <jirayu.majesty@mail.kmutt.ac.th>
2022-12-12 18:43:12 +0700

    น้ำตาจิไหล ในที่สุด

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ class Program
         int level = -1,countbranch = 0,count = 0,group = 0;
         Stack<string> sname = new Stack<string>();
         Stack<int> sbranch = new Stack<int>();
-        Tree<int> sgroup = new Tree<int>();
+        Stack<string> aname = new Stack<string>();
         Tree<int> slevel = new Tree<int>();
         Tree<string> tree = new Tree<string>();
         while(true)
@@ -18,9 +18,7 @@ class Program
             {
                 tree.AddChild(level,name);
                 slevel.AddChild(level,level);
-                sgroup.AddChild(level,group);
                 level++;
-                group++;
                 countbranch += branch;
                 sbranch.Push(branch);
                 if(tree.GetLength() > 1)
@@ -54,35 +52,35 @@ class Program
             }
         }
         string vacator = Console.ReadLine();
-        int compare = 0;
+        int compare = 0,tail = 0;
         for(int i=0; i<tree.GetLength(); i++)
         {
             if(vacator == tree.Get(i))
             {
-                compare = slevel.Get(i);
-                group = sgroup.Get(i);
+                tail = i; // tail bass = 2
+                compare = slevel.Get(i); // 1
             }
         }
-        for(int i=0; i<tree.GetLength(); i++)
+        while(compare > -1)
         {
-            if(slevel.Get(i) < compare)
+            for(int i=tail; i>=0 && compare > -1; i--)
             {
-                if(sgroup.Get(i) <= group)
+                if(slevel.Get(i) < compare)
                 {
                     sname.Push(tree.Get(i));
+                    tail = i;
+                    compare--;
+                    break;
                 }
             }
         }
-        // Check data
-        /*
-        for(int i=0; i<tree.GetLength(); i++)
-        {
-            Console.WriteLine("{0} {1} {2} ",tree.Get(i),sgroup.Get(i),slevel.Get(i));
-        }
-        */
         for(int i=0; i<sname.GetLength(); i++)
         {
-            Console.WriteLine(sname.Get(i));
+            aname.Push(sname.Get(i));
+        }
+        for(int i=0; i<sname.GetLength(); i++)
+        {
+            Console.WriteLine(aname.Get(i));
         }
     }
 }
